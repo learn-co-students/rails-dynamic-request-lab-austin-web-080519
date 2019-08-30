6f8ad985c6c43c2d9a5e86c6caa03af1e0d9956b
edwinKR <edwin.k.kim@gmail.com>
2019-08-30 08:14:24 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find_by_id(params[:id])
+    puts '==========='
+    puts @student
+  end
 end
